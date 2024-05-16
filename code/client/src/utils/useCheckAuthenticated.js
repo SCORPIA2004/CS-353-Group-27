52824946ff6b9824a3798228560c71a0c74c87aa
Zahaab Khawaja <zahaabz@gmail.com>
2024-05-16 17:30:54 +0300

    fix

--- code/client/src/utils/useCheckAuthenticated.js
+++ code/client/src/utils/useCheckAuthenticated.js
@@ -1,6 +1,5 @@
 const useCheckAuthenticated = () => {
     const isAuthenticated = localStorage.getItem('token') ? true : false;
-    const isLoading = false;
     return [isAuthenticated, true];
 }
 
