049b994360e01ef656dc1a7504138c4eb22dad1c
Zahaab Khawaja <zahaabz@gmail.com>
2024-05-16 20:19:57 +0300

    register complete

---
+++ code/client/src/helpers/ApiUrlHelper.js
@@ -0,0 +1,5 @@
+const SERVER_URL = 'http://localhost:3000';
+
+const USERS_URL = `${SERVER_URL}/users`;
+export const LOGIN_URL = `${USERS_URL}/login`;
+export const REGISTER_URL = `${USERS_URL}/register`;
