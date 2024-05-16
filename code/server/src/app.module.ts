d8669d5f70095dcf5bdb65fab1d4aa883c52b94d
Yassin Younis <yassin.younis@layermark.com>
2024-05-16 20:59:36 +0300

    Added leaderboard (heavy testing required)

--- code/server/src/app.module.ts
+++ code/server/src/app.module.ts
@@ -5,9 +5,10 @@ import {UsersModule} from "./users/users.module";
 import { WorkoutsModule } from '@/src/workouts/workouts.module';
 import { GoalsModule } from './goals/goals.module';
 import { TrainingModule } from './training/training.module';
+import { LeaderboardModule } from './leaderboard/leaderboard.module';
 
 @Module({
-  imports: [ConfiguredThrottler, UsersModule, WorkoutsModule, GoalsModule, TrainingModule],
+  imports: [ConfiguredThrottler, UsersModule, WorkoutsModule, GoalsModule, TrainingModule, LeaderboardModule],
   controllers: [AppController],
   providers: [],
 })
