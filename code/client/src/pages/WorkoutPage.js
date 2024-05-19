4bc7b2ff48827affcaaf51cb82ebffd3812f241c
Zahaab Khawaja <zahaabz@gmail.com>
2024-05-19 13:58:22 +0300

    update ui for workout and active workout

--- code/client/src/pages/WorkoutPage.js
+++ code/client/src/pages/WorkoutPage.js
@@ -1,8 +1,9 @@
 import React, {useState, useEffect} from 'react';
 import {Box, Button, Container, Flex, Heading, Text} from "@radix-ui/themes";
-import {WORKOUT_LOG_URL, WORKOUTS_URL} from "../helpers/ApiUrlHelper";
+import {WORKOUTS_URL} from "../helpers/ApiUrlHelper";
 import {useNavigate, useParams} from "react-router-dom";
 import useAuth from "../utils/useAuth";
+import styled from "styled-components";
 
 const WorkoutPage = () => {
     const [workout, setWorkout] = useState(null);
@@ -38,45 +39,96 @@ const WorkoutPage = () => {
         }
     }, [isLoading, isAuthenticated]);
 
-    const formatTime = (totalSeconds) => {
-        const hours = Math.floor(totalSeconds / 3600);
-        const minutes = Math.floor((totalSeconds % 3600) / 60);
-        const seconds = totalSeconds % 60;
+    const Title = styled.h2`
+        margin-bottom: 5px;
+    `;
 
-        const formattedHours = hours > 0 ? `${hours}h ` : '';
-        const formattedMinutes = minutes > 0 ? `${minutes}m ` : '';
-        const formattedSeconds = `${seconds}s`;
+    const Subtitle = styled.h5`
+        margin-bottom: 15px;
+        font-weight: normal;
+        font-size: 18px;
+    `;
 
-        return formattedHours + formattedMinutes + formattedSeconds;
-    };
+    const Details = styled.p`
+        font-size: 21px;
+        line-height: 1.5;
+        text-align: left;
+    `;
+
+    const Background = styled.div`
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        z-index: 1000;
+        backdrop-filter: blur(1.5px);
+    `;
+
+    const ModalWrapper = styled.div`
+        margin-top: 20px;
+        background: linear-gradient(145deg, #6a3093, #a044ff);
+        color: white;
+        width: 90%;
+        max-width: 400px;
+        border-radius: 10px;
+        padding: 20px;
+        box-shadow: 0 8px 16px rgba(0, 0, 0, 0.25);
+        display: flex;
+        flex-direction: column;
+        position: relative;
+    `;
+
+    const ContentArea = styled.div`
+        padding: 20px;
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+    `;
+
+// button is blue
+    const Button = styled.button`
+        padding: 10px 20px;
+        margin-top: 20px;
+        border: none;
+        border-radius: 5px;
+        background-color: #3f51b5;
+        color: white;
+        font-size: 21px;
+        cursor: pointer;
+        transition: background-color 0.3s;
+
+        &:hover {
+            background-color: #303f9f;
+        }
+    `;
 
     if (!workout) return <div>Loading...</div>;
 
     return (
         <Container style={{paddingInline: '10px'}}>
-            <Flex justify="center" direction="column" gap="4" py="4">
-                <Flex direction="row" justify="between">
-                    <Heading>Active Workout - {workout?.title}</Heading>
-                </Flex>
-
-                <Box>
-                    <Flex direction={"column"} gap={"1"}>
-                        <Flex direction={"row"} justify={"between"}>
-                            <Text>{workout?.duration} mins, {workout?.difficulty}</Text>
-                            <Text>Intensity: {workout?.intensity}</Text>
-                        </Flex>
-                        <Text size={"5"} style={{marginBlock: '20px'}}>Required Equipment: {workout?.required_equipment}</Text>
-                        <Text size={"5"}>{workout?.description}</Text>
-                    </Flex>
-                </Box>
-
-                <Flex justify="center">
-                    <Button style={{cursor: 'pointer'}}
-                            onClick={() => {navigate(`/active-workout/${id}`)}}>
-                        Start Workout
-                    </Button>
-                </Flex>
-            </Flex>
+                <Background>
+                    <ModalWrapper>
+                        <ContentArea>
+                            <>
+                                <Title>{workout.title}</Title>
+                                <Subtitle>
+                                    ⏰{workout.duration} mins, 💪{workout.difficulty}
+                                </Subtitle>
+                                <Subtitle style={{marginTop: '0'}}>
+                                    🏃 Equipment: {workout.required_equipment}
+                                </Subtitle>
+                                <Details>{workout.description}</Details>
+                                <Flex justify="center">
+                                    <Button style={{cursor: 'pointer'}}
+                                            onClick={() => {
+                                                navigate(`/active-workout/${id}`)
+                                            }}>
+                                        Start Workout
+                                    </Button>
+                                </Flex>
+                            </>
+                        </ContentArea>
+                    </ModalWrapper>
+                </Background>
         </Container>
     );
 };
