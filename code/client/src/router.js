8925cb855fa314db1b6e4e7c52b9cfd9e47da79e
kayaselcukmehmet <114066225+kayaselcukmehmet@users.noreply.github.com>
2024-05-19 10:23:00 +0300

    Update router.js
    Fixed merge error

--- code/client/src/router.js
+++ code/client/src/router.js
@@ -1,38 +1,33 @@
+@@ -1,31 +1,33 @@
 /**
  * Renders the router component for handling client-side routing.
  * @returns {JSX.Element} The router component.
  */
 import React from 'react';
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import {BrowserRouter, Routes, Route} from "react-router-dom";
 import NavBar from "./navbar";
-import IndexPage from "./pages";
 import RegisterPage from "./pages/register";
 import LoginPage from "./pages/login";
 import HomePage from "./pages";
-import LeaderboardPage from './pages/LeaderBoard';
-import WorkoutStartPage from './pages/WorkoutStartPage';
-import WorkoutInProgressPage from './pages/WorkoutInProgressPage';
-import WorkoutSummaryPage from './pages/WorkoutSummaryPage';
-import TrainerHomePage from './pages/TrainerHomepage';
-import CreateWorkoutPage from './pages/CreateWorkout';
+import WorkoutSelectionPage from "./pages/WorkoutSelectionPage";
+import ActiveWorkoutPage from "./pages/ActiveWorkoutPage";
+import WorkoutPage from "./pages/WorkoutPage";
+
 
 const Router = () => {
     return (
         <BrowserRouter>
-            <NavBar />
+            <NavBar/>
             <Routes>
-                <Route path="/" element={<HomePage />} />
-                <Route path="/login" element={<LoginPage />} />
-                <Route path="/register" element={<RegisterPage />} />
-                <Route path="/leaderBoard" element={<LeaderboardPage />} />
-                <Route path="/workoutStart/:id" element={<WorkoutStartPage />} />
-                <Route path="/workoutInProgress/:id" element={<WorkoutInProgressPage />} />
-                <Route path="/workoutSummary/:id" element={<WorkoutSummaryPage />} />
-                <Route path="/trainerHome" element={<TrainerHomePage />} />
-                <Route path="/createWorkout" element={<CreateWorkoutPage />} />
+                <Route path="/" element={<HomePage/>}/>
+                <Route path="/login" element={<LoginPage/>}/>
+                <Route path="/register" element={<RegisterPage/>}/>
+                <Route path="/workout-selection" element={<WorkoutSelectionPage/>}/>
+                <Route path="/active-workout/:id" element={<ActiveWorkoutPage />} />
+                <Route path="/workout/:id" element={<WorkoutPage />} />
             </Routes>
         </BrowserRouter>
-    );
+    )
 }
 
-export default Router;
+export default Router
