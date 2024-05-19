25774629661edaee256de1c5d990ca8e3effbd96
kayaselcukmehmet <114066225+kayaselcukmehmet@users.noreply.github.com>
2024-05-19 10:11:46 +0300

    Update login.js
    Merge error fixed sorry for that

--- code/client/src/pages/login.js
+++ code/client/src/pages/login.js
@@ -1,82 +1,88 @@
-import React, { useState } from 'react';
-import { Button, Card, Container, Flex, Heading, Text, TextField } from "@radix-ui/react-components";
-import { useNavigate } from "react-router-dom";
-
+import React, {useState} from 'react';
+import {Button, Card, Container, Flex, Heading, Text, TextField} from "@radix-ui/themes";
+import {useNavigate} from "react-router-dom";
+import {LOGIN_URL} from "../helpers/ApiUrlHelper";
+import useAuth from "../utils/useAuth";
 const LoginPage = () => {
   const navigate = useNavigate();
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
-  const [error, setError] = useState('');
-
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [error, setError] = useState('');
+    const {login} = useAuth();
   const handleLogin = async (event) => {
-    event.preventDefault();
+        event.preventDefault();
     try {
-      const response = await fetch('http://yourapi.com/login', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({ email, password }),
-      });
-
+            const response = await fetch(`${LOGIN_URL}`, {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify({email, password}),
+            });
       if (response.ok) {
-        const data = await response.json();
-        localStorage.setItem('token', data.token); 
+                const data = await response.text();
+                login(data)
 
-        //We should have role kind of thing here
-        const role = data.role;
-        
-        if (role === 'trainee') {
-          navigate('/');
-        } else if (role === 'trainer') {
-          navigate('/trainerHome');
+                navigate('/')
+            } else if (response.status === 400) {
+                setError('Invalid credentials. Please try again.');
+            } else {
+                setError('Failed to connect to the service. Please try again later.');
+            }
+        } catch (err) {
+            setError('Failed to connect to the service. Please try again later.');
         }
-      } else {
-        setError('Invalid credentials. Please try again.'); 
-      }
-    } catch (err) {
-      setError('Failed to connect to the service. Please try again later.'); 
-    }
-  };
+    };
 
-  return (
-    <div style={{
-      backgroundImage: "url('bg.jpg')", 
-      backgroundSize: 'cover',
-      backgroundPosition: 'center',
-      height: '100vh',  
-      width: '100vw'   
-    }}>
-      <Container>
-        <Flex justify='center' css={{ padding: '$5' }}>
-          <Card>
-            <form onSubmit={handleLogin}>
-              <Flex direction='column' css={{ padding: '$3' }} justify='center'>
-                <Heading css={{ margin: '$1', textAlign: 'center' }}>Login</Heading>
-                <Text as="div" css={{ color: '$gray11', fontSize: '$3' }}>
-                  Please enter your email and password.
-                </Text>
-                {error && (
-                  <Text as="div" css={{ color: '$red11', fontSize: '$3', textAlign: 'center' }}>
-                    {error}
-                  </Text>
-                )}
-                <Flex justify='center' direction='column' css={{ gap: '$3', marginTop: '$5' }}>
-                  <TextField.Root size="1" placeholder="Email" value={email} onValueChange={setEmail} />
-                  <TextField.Root size="1" placeholder="Password" type="password" value={password} onValueChange={setPassword} />
+
+    return (
+        <div style={{
+            height: 'calc(100vh - 52px)',
+            display: 'flex',
+            alignItems: 'center',
+            justifyContent: 'center',
+        }}>
+            <Container>
+                <Flex justify='center' p='5'>
+                    <Card>
+                        <form onSubmit={handleLogin}>
+                            <Flex direction='column' p='3' justify='center'>
+                                <Heading m='1' align='center'>Login</Heading>
+                                <Text as="div" color="gray" size="2" style={{textAlign: 'center'}}>
+                                    Please enter your email and password.
+                                </Text>
+                                {error && (
+                                    <Text as="div" color="red" size="2" style={{textAlign: 'center'}}>
+                                        {error}
+                                    </Text>
+                                )}
+                                <Flex justify='center' direction='column' gap='1' mt='5'>
+                                    <TextField.Root
+                                        placeholder="Email"
+                                        onChange={(e) => setEmail(e.target.value)}
+                                        required
+                                    />
+                                    <TextField.Root
+                                        placeholder="Password"
+                                        type="password"
+                                        onChange={(e) => setPassword(e.target.value)}
+                                        required
+                                    />
+                                </Flex>
+                                <Button my='4' type="submit">Login</Button>
+                                <Flex justify='center' align='center'>
+                                    <Text size='2'>New Here?</Text>
+                                    <Button variant='ghost' ml='5' onClick={() => navigate("/register")}
+                                            className={'cursor-pointer'}>Create
+                                        an account</Button>
+                                </Flex>
+                            </Flex>
+                        </form>
+                    </Card>
                 </Flex>
-                <Button css={{ marginTop: '$4' }} type="submit">Login</Button>
-                <Flex justify='center' align='center' css={{ marginTop: '$3' }}>
-                  <Text css={{ fontSize: '$2' }}>New Here?</Text>
-                  <Button variant='ghost' css={{ marginLeft: '$5' }} onClick={() => navigate("/register")}>Create an account</Button>
-                </Flex>
-              </Flex>
-            </form>
-          </Card>
-        </Flex>
-      </Container>
-    </div>
-  );
+ </Container>
+        </div>
+    );
 }
 
 export default LoginPage;
